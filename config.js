4093f828cead2763b8fab1757d0324c071447e40
David Sanchez <d.czanches@gmail.com>
2017-01-31 20:17:44 -0800

    Remove config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-exports.appCredentials = {
-    fitbit: {
-        client_id: '22849P',
-        auth_url: 'https://www.fitbit.com/oauth2/authorize',
-        scope: 'activity heartrate location nutrition profile settings sleep social weight',
-        redirect_uri: 'http://localhost:3000/fitbit-authorization'
-    }
-}
